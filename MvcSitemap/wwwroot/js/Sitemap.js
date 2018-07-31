d9d1db525ec036ea38d63543011d096a45342aad
larryhpham <larrypham1991@yahoo.com>
2018-07-31 02:14:38 -0500

    stuff

--- MvcSitemap/wwwroot/js/Sitemap.js
+++ MvcSitemap/wwwroot/js/Sitemap.js
@@ -4,19 +4,45 @@ $(function () {
         e.preventDefault();
 
         var formData = new FormData($(this)[0]);
-        console.log(formData);
-
         $.ajax({
             url: 'UploadFiles',
             type: 'POST',
             data: formData,
             async: false,
             success: function (data) {
-                console.log('success', data);
+                xmlCompare(data.xmlData, siteMapOriginal);
             },
             cache: false,
             contentType: false,
             processData: false
         })
     });
+
+    function xmlCompare(siteMapArray, originalData) {
+        console.log('success', siteMapArray);
+        var modifiedArray = [];
+        if (originalData.length === 0) {
+            siteMapArray.forEach(site => {
+                site.status = 'new';
+                modifiedArray.push(site);
+            });
+            return modifiedArray;
+        }
+
+        // check the original data to help set the flag of the siteMapArray for status
+        siteMapArray.forEach(sitemap => {
+            const status = 'new';
+
+            originalData.forEach(originalSite => {
+                if (sitemap.url !== originalData.Url) return;
+                originalSite.status = 'edit';
+                status = 'edit';
+            })
+
+            sitemap.status = status;
+            modifiedArray.push(sitemap);
+        })
+        console.log('MODIFIED ARRAY', modifiedArray);
+    }
+
 });
